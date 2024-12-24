d3c352efe33f851ded6ef6fd7125c4ef0ac84a6e
AbdulSamadNadeem <samadnadeem372@gmail.com>
2024-12-24 13:41:41 +0500

    updated

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -7,7 +7,7 @@ const RouteHandlers = require("../RouteHandlers/Routehandlers");
 Router.route("/getallstudents").get(RouteHandlers.getAllData);
 Router.route("/getstudentsByRollNumber/:roll").get(RouteHandlers.getDatabyRoll);
 Router.route("/addstudent").post(RouteHandlers.CreateData);
-Router.route("/updatestudent/:roll").get(RouteHandlers.updateData);
-Router.route("/deletestudent/:roll").get(RouteHandlers.DeleteData);
+Router.route("/updatestudent/:roll").patch(RouteHandlers.updateData);
+Router.route("/deletestudent/:roll").delete(RouteHandlers.DeleteData);
 
 module.exports = Router;
